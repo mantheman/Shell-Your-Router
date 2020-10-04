908397c705f2513b0a3e23d5e88b6e810c22c733
Yuval Krinsky <ykrinsky@gmail.com>
2020-10-04 12:49:44 +0300

    Changed logger module to have one global logger.

--- code/backdoor/include/logger.h
+++ code/backdoor/include/logger.h
@@ -11,10 +11,6 @@
 
 #define MAX_LOG_MESSAGE_SIZE (200)
 
-typedef struct logger__log_s {
-    FILE * log_file;
-} logger__log_t;
-
 typedef enum log_level_e{
     LOG_DEBUG = 0,
     LOG_INFO,
@@ -24,30 +20,26 @@ typedef enum log_level_e{
 } log_level_t;
 
 /**
- * @brief Initialises logger.
+ * @brief Initialises the program's logger.
  * 
  * @param log_path [in] File path of the new logger.
- * @param new_logger [out] Pointer that will hold the new logger.
  * @return return_code_t 
- * @note new_logger should already be allocated.
  */
-return_code_t logger__init_logger(const char *log_path, logger__log_t *new_logger);
+return_code_t logger__init_logger(const char *log_path);
 
 /**
- * @brief Frees given logger resources.
+ * @brief Frees the program's logger resources.
  * 
- * @param logger [in] Logger which resources will be freed.
  * @return return_code_t 
  */
-return_code_t logger__destory_logger(logger__log_t *logger);
+return_code_t logger__destory_logger();
 
 /**
  * @brief Log new message to the given log.
  * 
- * @param logger [in] New message will be logged to this log.
  * @param log_level [in] Log level of given message.
  * @param message [in] Message to be printed to the log.
  * @return return_code_t 
  * @note Message should be no longer than MAX_LOG_MESSAGE_SIZE bytes.
  */
-return_code_t logger__log(const logger__log_t *logger, log_level_t log_level, char *message);
+return_code_t logger__log(log_level_t log_level, char *message);
--- code/backdoor/src/logger.c
+++ code/backdoor/src/logger.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE (199506L) // For flockfile and funlockfile functions.
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "return_codes.h"
 #include "common.h"
@@ -11,13 +12,23 @@
 
 static char log_levels_g[][LOG_LEVEL_MAX_SIZE] = {"DEBUG", "INFO", "WARNING", "CRITICAL", "ERROR"};
 
+typedef struct logger__log_s {
+    FILE * log_file;
+} logger__log_t;
 
-return_code_t logger__init_logger(const char *log_path, logger__log_t *new_logger)
+// Global logger of the program.
+static logger__log_t *g_logger = NULL;
+
+return_code_t logger__init_logger(const char *log_path)
 {
     return_code_t result = RC_UNINITIALIZED;
     FILE *new_log_file = NULL;
 
-    if (NULL == log_path || NULL == new_logger){
+    if (NULL != g_logger){
+        handle_error(RC_LOGGER__INIT_LOGGER__LOGGER_ALREADY_INITIALIZED);
+    }
+
+    if (NULL == log_path){
         handle_error(RC_LOGGER__INIT_LOGGER__BAD_PARAMS);
     }
 
@@ -26,50 +37,63 @@ return_code_t logger__init_logger(const char *log_path, logger__log_t *new_logge
         handle_perror("Fopen failed", RC_LOGGER__INIT_LOGGER__FOPEN_FAILED);
     }
 
-    new_logger->log_file = new_log_file;
+    g_logger = malloc(sizeof(*g_logger));
+    if (NULL == g_logger){
+        handle_perror("Malloc failed", RC_LOGGER__INIT_LOGGER__MALLOC_FAILED);
+    }
+
+    g_logger->log_file = new_log_file;
+
     result = RC_SUCCESS;
 l_cleanup:
     return result;
 }
 
-return_code_t logger__destory_logger(logger__log_t *logger)
+return_code_t logger__destory_logger()
 {
     return_code_t result = RC_UNINITIALIZED;
 
-    if (NULL == logger){
-        handle_error(RC_LOGGER__DESTROY_LOGGER__BAD_PARAMS);
+    if (NULL == g_logger){
+        handle_error(RC_LOGGER__DESTROY_LOGGER__LOGGER_NOT_INITIALIZED);
     }
 
-    if (EOF == fclose(logger->log_file)){
+    if (EOF == fclose(g_logger->log_file)){
         handle_perror("Fclose failed", RC_LOGGER__DESTROY_LOGGER__FCLOSE_FAILED);
     }
+
+    free(g_logger);
+    g_logger = NULL;
 
     result = RC_SUCCESS;
 l_cleanup:
     return result;
 }
 
-return_code_t logger__log(const logger__log_t *logger, log_level_t log_level, char *message)
+return_code_t logger__log(log_level_t log_level, char *message)
 {
     return_code_t result = RC_UNINITIALIZED;
     char log_message[FINAL_LOG_MESSAGE_MAX_SIZE] = {0};
     char *log_level_string = NULL;
 
-    if (NULL == logger || NULL == message){
+    if (NULL == g_logger){
+        handle_error(RC_LOGGER__DESTROY_LOGGER__LOGGER_NOT_INITIALIZED);
+    }
+
+    if (NULL == message){
         handle_error(RC_LOGGER__LOG__BAD_PARAMS);
     }
 
     log_level_string = log_levels_g[(int)log_level];
     snprintf(log_message, FINAL_LOG_MESSAGE_MAX_SIZE, "%s: %s\n", log_level_string, message);
 
-    flockfile(logger->log_file);
+    flockfile(g_logger->log_file);
 
-    fwrite(log_message, strlen(log_message), 1, logger->log_file);
-    if (EOF == fflush(logger->log_file)){
+    fwrite(log_message, strlen(log_message), 1, g_logger->log_file);
+    if (EOF == fflush(g_logger->log_file)){
         handle_perror("Fflush failed", RC_LOGGER__LOG__FFLUSH_FAILED);
     }
 
-    funlockfile(logger->log_file);
+    funlockfile(g_logger->log_file);
 
     result = RC_SUCCESS;
 l_cleanup:
